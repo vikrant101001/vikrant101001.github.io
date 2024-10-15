4189a358279d3b040c5c8d286b5d27a259dda46c
vikrant101001 <vikrant.th2013@gmail.com>
2024-10-15 10:24:40 +0530

    Add new ReactJS portfolio codebase

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import React, { Component } from "react";
+import $ from "jquery";
+import "./App.scss";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+
+class App extends Component {
+
+  constructor(props) {
+    super();
+    this.state = {
+      foo: "bar",
+      resumeData: {},
+      sharedData: {},
+    };
+  }
+
+  applyPickedLanguage(pickedLanguage) {
+    document.documentElement.lang = pickedLanguage;
+    var resumePath = `res_primaryLanguage.json`; // Always load the primary language resume
+    this.loadResumeFromPath(resumePath);
+  }
+
+  componentDidMount() {
+    this.loadSharedData();
+    this.applyPickedLanguage(window.$primaryLanguage); // Only use primary language
+  }
+
+  loadResumeFromPath(path) {
+    $.ajax({
+      url: path,
+      dataType: "json",
+      cache: false,
+      success: function (data) {
+        this.setState({ resumeData: data });
+      }.bind(this),
+      error: function (xhr, status, err) {
+        alert(err);
+      },
+    });
+  }
+
+  loadSharedData() {
+    $.ajax({
+      url: `portfolio_shared_data.json`,
+      dataType: "json",
+      cache: false,
+      success: function (data) {
+        this.setState({ sharedData: data });
+        document.title = `${this.state.sharedData.basic_info.name}`;
+      }.bind(this),
+      error: function (xhr, status, err) {
+        alert(err);
+      },
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header sharedData={this.state.sharedData.basic_info} />
+        {/* Language selection removed, only primary language remains */}
+        <About
+          resumeBasicInfo={this.state.resumeData.basic_info}
+          sharedBasicInfo={this.state.sharedData.basic_info}
+        />
+        <Projects
+          resumeProjects={this.state.resumeData.projects}
+          resumeBasicInfo={this.state.resumeData.basic_info}
+        />
+        <Skills
+          sharedSkills={this.state.sharedData.skills}
+          resumeBasicInfo={this.state.resumeData.basic_info}
+        />
+        <Experience
+          resumeExperience={this.state.resumeData.experience}
+          resumeBasicInfo={this.state.resumeData.basic_info}
+        />
+        <Footer sharedBasicInfo={this.state.sharedData.basic_info} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import Typical from "react-typical";
+
+class Header extends Component {
+  titles = [];
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    if (this.props.sharedData) {
+      var name = this.props.sharedData.name;
+      this.titles = this.props.sharedData.titles.map(x => [ x.toUpperCase(), 1500 ]).flat();
+    }
+
+    const HeaderTitleTypeAnimation = React.memo(() => {
+      return <Typical className="title-styles" steps={this.titles} loop={50} />;
+    }, (props, prevProp) => true);
+
+    return (
+      <header id="home" style={{ height: window.innerHeight - 140, display: 'block' }}>
+        <div className="row aligner" style={{ height: '100%' }}>
+          <div className="col-md-12">
+            <div>
+              <span className="iconify header-icon" data-icon="la:laptop-code" data-inline="false"></span>
+              <br />
+              <h1 className="mb-0">
+                <Typical steps={[name]} wrapper="p" />
+              </h1>
+              <div className="title-container">
+                <HeaderTitleTypeAnimation />
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
