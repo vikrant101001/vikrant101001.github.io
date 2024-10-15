6419b74efaa214a98cf37dcc4ccfc958d4e06bf9
vikrant101001 <vikrant.th2013@gmail.com>
2024-10-15 10:27:13 +0530

    Updates

---
+++ precache-manifest.18bfbecd18e6ccf80a33cf9e366619bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e5ba782b0deeef23bbf30b0ec7397b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3af683bd42ce716f44a0",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "7a0335df3eed85e12d00",
+    "url": "/static/css/main.d0c8cb44.chunk.css"
+  },
+  {
+    "revision": "3af683bd42ce716f44a0",
+    "url": "/static/js/2.b9a8b523.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.b9a8b523.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a0335df3eed85e12d00",
+    "url": "/static/js/main.a4b2cd34.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
